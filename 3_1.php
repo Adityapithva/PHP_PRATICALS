766a636ea774b2998476238ce47d98e2b3398b64
Adityapithva <121885084+Adityapithva@users.noreply.github.com>
2024-02-06 12:04:57 +0530

    Changes in 3_1

--- 3_1.php
+++ 3_1.php
@@ -1,24 +1,12 @@
-<?php
-if($_POST['select'] == 'add'){
-    echo"Addition:".$_POST['a'] + $_POST['b'];
-}
-elseif($_POST['select'] == 'sub'){
-    echo 'Subtraction:'.$_POST['a'] - $_POST['b'];
-}
-elseif($_POST['select'] == 'mul'){
-    echo 'Multiplication:'.$_POST['a'] * $_POST['b'];
-}
-else{
-    echo"Division:".$_POST['a'] / $_POST['b'];
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculator</title>
 </head>
+
 <body>
     <h1>Simple Calculator</h1>
     <form action="3_1.php" method="post">
@@ -31,7 +19,20 @@ else{
             <option value="mul">Multiplication</option>
             <option value="div">Division</option><br>
         </select>
+        <br>
         <input type="submit" value="submit">
     </form>
+    <?php
+    if ($_POST['select'] == 'add') {
+        echo "Addition:" . $_POST['a'] + $_POST['b'];
+    } elseif ($_POST['select'] == 'sub') {
+        echo 'Subtraction:' . $_POST['a'] - $_POST['b'];
+    } elseif ($_POST['select'] == 'mul') {
+        echo 'Multiplication:' . $_POST['a'] * $_POST['b'];
+    } else {
+        echo "Division:" . $_POST['a'] / $_POST['b'];
+    }
+    ?>
 </body>
+
 </html>
